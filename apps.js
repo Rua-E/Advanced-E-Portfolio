cca617bcb279154d401c828b9787b6cd3e0ea7db
Rua <rua.elkasheef@gmail.com>
2024-02-12 23:15:00 -0500

    completing javascript and starting @media's

--- apps.js
+++ apps.js
@@ -1,3 +1,5 @@
+// MODAL INSTRUCTIONS
+
 // template_yap15t8
 // service_n89uqzu
 // 0epMskdZt5pVrq72D
@@ -39,6 +41,9 @@
         }, 500);
      }
      
+// TOGGLING INSTRUCTIONS
+
+
 let isModalOpen = false;
 function toggleModal(){
     if (isModalOpen) {
@@ -47,4 +52,32 @@ function toggleModal(){
     }
     isModalOpen = true;
     document.body.classList+= " modal--open"
+}
+
+
+
+let contrastToggle = false;
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+// BACKGROUND EFFECTS INSTRUCTIONS
+
+const scaleFactor = 1/20
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i ){
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y *boolInt}px)`
+    }
 }
